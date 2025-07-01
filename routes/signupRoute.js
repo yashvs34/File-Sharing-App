b97730688f91c306f7343ba0a1a9257ebdd05703
Harshvardhan Singh <yashvardhansingh232@gmail.com>
2025-07-01 14:03:13 +0530

    Added express dependency

--- routes/signupRoute.js
+++ routes/signupRoute.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const router = express.Router();
 const signupValidator = require('../middlewares/inputValidator');
 const saveUser = require('../repository/userRepository');
