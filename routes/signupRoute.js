0efa63936b44e693d649a2f2733f4acf6af6d40a
Harshvardhan Singh <yashvardhansingh232@gmail.com>
2025-07-01 10:40:25 +0530

    Removed rendundant function call from repository layer

--- routes/signupRoute.js
+++ routes/signupRoute.js
@@ -1,12 +1,13 @@
 const router = express.Router();
 const signupValidator = require('../middlewares/inputValidator');
 const saveUser = require('../repository/userRepository');
+const findUser = require('../repository/userRepository');
 const hashPassword = require('../service/passwordHashing');
 
 router.post('/signup', signupValidator, async (req, res) => {
     try
     {
-        if (findAlreadyExistingUser({userName}))
+        if (findUser({userName}))
         {
             res.send('User already exists');
         }
