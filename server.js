eb58ae07106eb29e4ba51f8dd38daf51d78b068d
Harshvardhan Singh <yashvardhansingh232@gmail.com>
2025-07-01 10:49:17 +0530

    Added signin and signup route

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const connectDB = require('./configs/mongodbConnections');
-connectDB();
 const fileUploadRoute = require('./routes/fileUploadRoute');
 const sendMailRoute = require('./routes/sendMailRoute');
+const signupRoute = require('./routes/signupRoute');
+const signinRoute = require('./routes/signinRoute');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
+    connectDB();
     res.send("Welcome to File-Sharing-App");
 });
 
+app.use('/', signupRoute);
+app.use('/', signinRoute);
 app.use('/', fileUploadRoute);
 app.use('/', sendMailRoute);
 
