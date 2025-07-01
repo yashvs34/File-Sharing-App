838b8995d385528ccdf9ee42b6a6dccdaa54925f
Harshvardhan Singh <yashvardhansingh232@gmail.com>
2025-07-01 10:28:09 +0530

    Added password hashing logic using bcrypt

--- service/passwordHashing.js
+++ service/passwordHashing.js
@@ -1,2 +1,16 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
+
+function hashPassword (password)
+{
+    bcrypt.hash(password, saltRounds, (err, result) => {
+        if (!err)
+        {
+            return result;
+        }
+
+        console.log('Error while hashing password', err);
+    });
+}
+
+module.exports = hashPassword;
