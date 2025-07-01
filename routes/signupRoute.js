3bd0da7b28334cf2ee6eacc00ffc682a4dbbfefb
Harshvardhan Singh <yashvardhansingh232@gmail.com>
2025-07-01 10:32:36 +0530

    Added logic to save hashed password in DB

--- routes/signupRoute.js
+++ routes/signupRoute.js
@@ -1,6 +1,7 @@
 const router = express.Router();
 const signupValidator = require('../middlewares/inputValidator');
 const saveUser = require('../repository/userRepository');
+const hashPassword = require('../service/passwordHashing');
 
 router.post('/signup', signupValidator, async (req, res) => {
     try
@@ -10,6 +11,8 @@ router.post('/signup', signupValidator, async (req, res) => {
             res.send('User already exists');
         }
         
+        const hashedPassword = hashPassword(password);
+
         await saveUser({userName, hashedPassword, firstName, lastName});
         
         res.send('Account created successfully');
