a126e9c3f09afed954d953d9bc61cde3e3fd6bf6
yashvs34 <yashvardhansingh232@gmail.com>
2025-09-16 00:13:43 +0530

    Changed from auto resource type in cloudinary to raw

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -34,7 +34,7 @@ function Dashboard ()
 
     return (
         <>
-            <div style={{ width: '100%', height: 600, position: 'fixed', zIndex: 0 }}>
+            <div style={{ width: '100vw', height: '100vh', position: 'fixed', zIndex: 0 }}>
                 <LiquidEther colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]} mouseForce={15} cursorSize={100} isViscous={false} viscous={30} iterationsViscous={32} iterationsPoisson={32} resolution={0.5} isBounce={false} autoDemo={true} autoSpeed={0.5} autoIntensity={2.2} takeoverDuration={0.25} autoResumeDelay={3000} autoRampDuration={0.6} />
             </div>
 
